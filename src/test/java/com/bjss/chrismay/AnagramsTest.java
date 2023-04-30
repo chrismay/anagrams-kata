f47fb256753706bb75b74760e43bed2f57109691
Chris May <chris.may@bjss.com>
2023-04-30 21:44:18 +0000

    clean up format

--- src/test/java/com/bjss/chrismay/AnagramsTest.java
+++ src/test/java/com/bjss/chrismay/AnagramsTest.java
@@ -4,30 +4,31 @@ import org.junit.jupiter.api.Test;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 public class AnagramsTest {
 
     @Test
-    public void canListAnagrams(){
-        var words = Set.of("tried", "tired","ohms","mohs","test");
+    public void canListAnagrams() {
+        var words = Set.of("tried", "tired", "ohms", "mohs", "test");
         var anags = Anagrams.anagramize(words);
 
-        // expect to get back two sets of anagrams; tried/tired and ohms/mohs. "test" has no anagrams so is not included in the result
+        // expect to get back two sets of anagrams; tried/tired and ohms/mohs. "test"
+        // has no anagrams so is not included in the result
         assertEquals(2, anags.size());
 
-        assertEquals(Set.of("tried", "tired"),anags.get("deirt"));
-        assertEquals(Set.of("ohms", "mohs"),anags.get("hmos"));
-
+        assertEquals(Set.of("tried", "tired"), anags.get("deirt"));
+        assertEquals(Set.of("ohms", "mohs"), anags.get("hmos"));
 
     }
 
     @Test
-    public void normalizeWord(){
+    public void normalizeWord() {
         var word = "hello";
         assertEquals("ehllo", Anagrams.normalizeWord(word));
     }
 
     @Test
-    public void anagramsAreKeyedByNormalisedForm(){
+    public void anagramsAreKeyedByNormalisedForm() {
         var words = Set.of("tried", "tired");
         var anags = Anagrams.anagramize(words);
         var expectedNormalisedForm = "deirt";
@@ -36,9 +37,10 @@ public class AnagramsTest {
     }
 
     @Test
-    public void quoteMarksAreSignificant(){
+    public void quoteMarksAreSignificant() {
 
-        // Quote marks count in the formation of an anagram, so these two are not anagrams of each other
+        // Quote marks count in the formation of an anagram, so these two are not
+        // anagrams of each other
         var words = Set.of("ohms", "ohm's");
         var anags = Anagrams.anagramize(words);
 
@@ -46,9 +48,10 @@ public class AnagramsTest {
     }
 
     @Test
-    public void accentsAreSignificant(){
+    public void accentsAreSignificant() {
 
-        // Accents count in the formation of an anagram, so these two are not anagrams of each other
+        // Accents count in the formation of an anagram, so these two are not anagrams
+        // of each other
         var words = Set.of("peat", "paté");
         var anags = Anagrams.anagramize(words);
 
